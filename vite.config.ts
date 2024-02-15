7afebc8e51fa34f568a13adc3f848cd076e4c65d
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2024-02-16 01:00:30 +0700

    fix relative path after build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		// https: true,
 		port: 3000,
+		watch: {
+			usePolling: true,
+		},
 	},
-	base: "",
+	base: "/",
 	resolve: {
 		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+	},
+	build: {
+		outDir: "./dist/",
 	},
 });
