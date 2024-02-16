81bbe94850c8f5f6f4473a049e27583505158aa2
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2024-02-16 15:30:32 +0700

    hotfix: fix re-render email verification page

--- src/hooks/useEmailVerification.tsx
+++ src/hooks/useEmailVerification.tsx
@@ -1,24 +1,31 @@
 import authStore from "@/zustand/auth.store";
+import { useLoadingStore } from "@/zustand/loading.store";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const useEmailVerification = () => {
-	const { search } = useLocation();
 	const [isVerified, setVerified] = useState<boolean>(false);
+	const { search } = useLocation();
+	const { isLoading, setLoading } = useLoadingStore((state) => state);
 	const { verifyEmailConfirmation } = authStore((state) => state);
 
 	useEffect(() => {
+		setLoading(true);
 		const params = new URLSearchParams(search);
 		const token = params.get("token");
 		const email = params.get("email");
+
 		const fetchData = async () => {
 			const response = await verifyEmailConfirmation({ email: email!, token: token! });
 			setVerified(response.isSuccess);
+			setLoading(false);
 		};
-		fetchData();
-	});
 
-	return isVerified;
+		fetchData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return { isLoading, isVerified };
 };
 
 export default useEmailVerification;
--- src/ui/pages/verify-email.page.tsx
+++ src/ui/pages/verify-email.page.tsx
@@ -3,14 +3,14 @@ import AppString from "@/utils/app-string";
 import { Typography } from "antd";
 
 export default function VerifyEmail() {
-	const isVerified = useEmailVerification();
+	const { isLoading, isVerified } = useEmailVerification();
 	return (
 		<div className="mx-auto flex-1 flex-col justify-center space-y-10  px-6 py-12 lg:px-8">
 			<Typography className="text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
 				Email Confirmation
 			</Typography>
 			<Typography className="text-center text-lg text-xl font-bold leading-9 tracking-tight text-gray-900">
-				{isVerified ? (
+				{isVerified && !isLoading ? (
 					<>
 						Thanks for your confirmation, your email has been verified successfully.
 						<a href="/sign-in" className="!hover:text-gray-100 ">
