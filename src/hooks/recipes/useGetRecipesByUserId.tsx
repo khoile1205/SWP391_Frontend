5672eac4067335a1df8406ff592c581d0eba9428
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2024-02-29 15:56:15 +0700

    sprint3: PP-58 + PP-59: UI + implement API for User Profile

--- src/hooks/recipes/useGetRecipesByUserId.tsx
+++ src/hooks/recipes/useGetRecipesByUserId.tsx
@@ -2,33 +2,43 @@ import { Recipe } from "@/models/recipe.model";
 import { recipeStore } from "@/zustand/recipe.store";
 import { useEffect, useState } from "react";
 
+const recipesPerPage = 3;
+
 export const useGetRecipesByUserId = (userId: string | undefined, filter: "newest" | "oldest") => {
 	const [visibleRecipes, setVisibleRecipes] = useState<Recipe[]>([]);
-	const [page, setPage] = useState(1); // Track the current page
-	const [hasMore, setHasMore] = useState(true); // Track if there are more recipes to load
+	const [page, setPage] = useState(1);
+	const [hasMore, setHasMore] = useState(true);
 
 	const { getRecipesByUserId } = recipeStore((state) => state);
 
 	const fetchData = async () => {
-		if (userId == undefined) return;
-		const response = await getRecipesByUserId(userId);
-		if (response.isSuccess) {
-			let userRecipes = response.data;
+		try {
+			if (userId == undefined) return;
 
-			if (filter === "newest") {
-				userRecipes = userRecipes.sort(
-					(a: Recipe, b: Recipe) =>
-						new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-				);
-			} else {
-				userRecipes = userRecipes.sort(
-					(a: Recipe, b: Recipe) =>
-						new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-				);
+			const response = await getRecipesByUserId(userId);
+
+			if (response.isSuccess) {
+				let userRecipes = response.data;
+
+				if (filter === "newest") {
+					userRecipes = userRecipes.sort(
+						(a: Recipe, b: Recipe) =>
+							new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+					);
+				} else {
+					userRecipes = userRecipes.sort(
+						(a: Recipe, b: Recipe) =>
+							new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+					);
+				}
+
+				const updatedRecipes = [...userRecipes.slice(0 * recipesPerPage, page * recipesPerPage)];
+
+				setVisibleRecipes(updatedRecipes);
+				setHasMore(updatedRecipes.length < userRecipes.length);
 			}
-
-			setHasMore(visibleRecipes.length > userRecipes.length);
-			setVisibleRecipes(() => [...userRecipes.slice(0, page * 5)]);
+		} catch (error) {
+			console.error("Error fetching recipes:", error);
 		}
 	};
 
@@ -37,10 +47,11 @@ export const useGetRecipesByUserId = (userId: string | undefined, filter: "newes
 			setPage((prevPage) => prevPage + 1);
 		}
 	};
+
 	useEffect(() => {
 		fetchData();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [page]);
+	}, [page, filter]);
 
 	return { visibleRecipes, handleLoadMore, hasMore };
 };
