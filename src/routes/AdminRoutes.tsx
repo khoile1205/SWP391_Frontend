524da78713d88aa57ab93f65f9dda88f90b0bfd6
Le Dinh Khoi <112401775+khoile1205@users.noreply.github.com>
2024-03-05 22:05:34 +0700

    sprint 3: PP-78: admin layout (#26)

---
+++ src/routes/AdminRoutes.tsx
@@ -0,0 +1,72 @@
+import RBACRoutes from "@/HOCs/RBACRoutes";
+import { Roles } from "@/enums";
+import { Loadable } from "@/ui/components/Loadable";
+import { MemoizedAdminLayout } from "@/ui/pages/admin/admin.layout.page";
+import { lazy } from "react";
+import { Navigate, RouteObject } from "react-router-dom";
+
+const AdminDashboardPage = Loadable(lazy(() => import("@/ui/pages/admin/admin.dashboard.page")));
+const AdminRecipePage = Loadable(lazy(() => import("@/ui/pages/admin/admin.recipe.page")));
+const AdminReportPage = Loadable(lazy(() => import("@/ui/pages/admin/admin.report.page")));
+const AdminTransactionPage = Loadable(
+	lazy(() => import("@/ui/pages/admin/admin.transaction.page"))
+);
+const AdminBookingPage = Loadable(lazy(() => import("@/ui/pages/admin/admin.booking.page")));
+const AdminRequestPage = Loadable(
+	lazy(() => import("@/ui/pages/admin/admin.become-chef-request.page"))
+);
+const AdminUserPage = Loadable(lazy(() => import("@/ui/pages/admin/admin.user.page")));
+const AdminChangePasswordPage = Loadable(
+	lazy(() => import("@/ui/pages/admin/admin.change-password.page"))
+);
+
+const AdminRoutes: RouteObject[] = [
+	{
+		path: "/admin",
+		element: (
+			<RBACRoutes allowedRoles={[Roles.ADMIN]}>
+				<MemoizedAdminLayout></MemoizedAdminLayout>
+			</RBACRoutes>
+		),
+		children: [
+			{
+				path: "",
+				element: <Navigate to="./dashboard"></Navigate>,
+			},
+			{
+				path: "dashboard",
+				element: <AdminDashboardPage />,
+			},
+			{
+				path: "change-password",
+				element: <AdminChangePasswordPage />,
+			},
+			{
+				path: "user",
+				element: <AdminUserPage />,
+			},
+			{
+				path: "recipe",
+				element: <AdminRecipePage />,
+			},
+			{
+				path: "report",
+				element: <AdminReportPage />,
+			},
+			{
+				path: "transaction",
+				element: <AdminTransactionPage />,
+			},
+			{
+				path: "booking",
+				element: <AdminBookingPage />,
+			},
+			{
+				path: "request",
+				element: <AdminRequestPage />,
+			},
+		],
+	},
+];
+
+export default AdminRoutes;
--- src/ui/components/Header/index.tsx
+++ src/ui/components/Header/index.tsx
@@ -9,6 +9,7 @@ import { LoginOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import { SearchBox } from "..";
 import { useLoadingCallback } from "@/hooks/common";
+import { Roles } from "@/enums";
 
 const navigation = [
 	{ name: "Home", href: "/", current: true },
@@ -126,19 +127,36 @@ export function Header() {
 														static
 														className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 													>
-														<Menu.Item>
-															{({ active }) => (
-																<a
-																	href="/profile/"
-																	className={classNames(
-																		active ? "bg-primary-100" : "",
-																		"block px-4 py-2 text-sm text-gray-700"
-																	)}
-																>
-																	Your Profile
-																</a>
-															)}
-														</Menu.Item>
+														{user.role == Roles.ADMIN ? (
+															<Menu.Item>
+																{({ active }) => (
+																	<a
+																		href="/admin"
+																		className={classNames(
+																			active ? "bg-primary-100" : "",
+																			"block px-4 py-2 text-sm text-gray-700"
+																		)}
+																	>
+																		View Dashboard
+																	</a>
+																)}
+															</Menu.Item>
+														) : (
+															<Menu.Item>
+																{({ active }) => (
+																	<a
+																		href="/profile/"
+																		className={classNames(
+																			active ? "bg-primary-100" : "",
+																			"block px-4 py-2 text-sm text-gray-700"
+																		)}
+																	>
+																		Your Profile
+																	</a>
+																)}
+															</Menu.Item>
+														)}
+
 														<Menu.Item>
 															{({ active }) => (
 																<a
