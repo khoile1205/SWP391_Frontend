8d1755dd73d62742161eba7a5ae0fad7593fabce
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2024-03-07 11:37:05 +0700

    add google analyze

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import "@/assets/css/index.css";
 import App from "./App";
 import "reflect-metadata";
 import QueryClientProvider from "./libs/query-client-provider";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-G0WE49QVVW");
+
+ReactGA.send({
+	hitType: "pageView",
+	page: window.location.pathname,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
